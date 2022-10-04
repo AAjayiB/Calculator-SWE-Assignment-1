b696415edcee5e6629383fae6b10a380b6424483
AAjayiB <72974478+AAjayiB@users.noreply.github.com>
2022-10-04 11:42:22 +0100

    added test for 0s and precedence tests

--- calculatorTests.py
+++ calculatorTests.py
@@ -1,6 +1,11 @@
 from calculator import calculate
 
 def testCalculate():
+    # Tests for string of 0s
+    assert calculate("0+0+0+0")=="0"
+    assert calculate("0-0-0-0")=="0"
+    assert calculate("0*0*0*0")=="0" 
+    
     # Tests for addition
     assert calculate("1+2+3+4")=="10"
     
@@ -8,6 +13,9 @@ def testCalculate():
     assert calculate("1*2*3*4")=="24"
     
     # Tests for subtraction
-    assert calculate("30-5-4-3")=="18"
+    assert calculate("6-5-4-3")=="-6"
 
-    
+    # Tests for precedence of combined operations
+    assert calculate("6*2-7+4")="9"
+    assert calculate("8+1*1-9")="0"
+    assert calculate("99-24+9*4")="111"
