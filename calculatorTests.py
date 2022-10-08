bdcef66f88b724034f6fe954564b98b96ffba1a7
AAjayiB <72974478+AAjayiB@users.noreply.github.com>
2022-10-08 22:26:34 +0100

    fixed tests

--- calculatorTests.py
+++ calculatorTests.py
@@ -2,7 +2,7 @@ from calculator import calculate
 
 def testCalculate():
     # Test for no string input
-    assert calculate("")="No input was entered."
+    assert calculate("")=="No input was entered."
 
     # Tests for string of 0s
     assert calculate("0+0+0+0")=="0"
@@ -15,9 +15,9 @@ def testCalculate():
     assert calculate("6-5-4-3")=="-6"
 
     # Tests for precedence of combined operations
-    assert calculate("6*2-7+4")="9"
-    assert calculate("8+1*1-9")="0"
-    assert calculate("99-24+9*4")="111"
+    assert calculate("6*2-7+4")=="9"
+    assert calculate("8+1*1-9")=="0"
+    assert calculate("99-24+9*4")=="111"
 
     # Tests for invalid numbers
     assert calculate("x+1")== "x is not a valid number"
