7ea695dc2d5c4258ab94f10add0e0d73dd8e9ee0
AAjayiB <72974478+AAjayiB@users.noreply.github.com>
2022-10-11 21:33:13 +0100

    tests made more general

--- calculatorTests.py
+++ calculatorTests.py
@@ -2,12 +2,7 @@ from calculator import calculate
 
 def testCalculate():
     # Test for no string input
-    assert calculate("")=="No input was entered."
-
-    # Tests for string of 0s
-    assert calculate("0+0+0+0")=="0"
-    assert calculate("0-0-0-0")=="0"
-    assert calculate("0*0*0*0")=="0" 
+    assert calculate("")=="No input was entered." 
 
     # Tests for basic computations
     assert calculate("1+2+3+4")=="10"
@@ -20,14 +15,14 @@ def testCalculate():
     assert calculate("99-24+9*4")=="111"
 
     # Tests for invalid numbers
-    assert calculate("x+1")== "x is not a valid number"
-    assert calculate("p-1")== "p is not a valid number"
-    assert calculate("a*1")== "a is not a valid number"
+    assert calculate("x+1")== "Not a valid input"
+    assert calculate("p-1")== "Not a valid input"
+    assert calculate("a*1")== "Not a valid input"
 
     # Tests for invalid operations
-    assert calculate("1/2")=="/ is not a valid operator"
-    assert calculate("1x2")=="x is not a valid operator"
-    assert calculate("1=2")=="= is not a valid operator"
+    assert calculate("1/2")=="Not a valid input"
+    assert calculate("1x2")=="Not a valid input"
+    assert calculate("1=2")=="Not a valid input"
 
     # Tests for strings beginning and ending with an operation
     assert calculate("1+2+3+")=="Expression cannot end with an operation"
